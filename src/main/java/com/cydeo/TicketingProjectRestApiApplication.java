d040ab184092474670d277f5bebadc708dd77136
Abdellatif Zouliga <zuuliga@gmail.com>
2023-12-18 10:32:26 -0500

    project created

---
+++ src/main/java/com/cydeo/TicketingProjectRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectRestApiApplication.class, args);
+    }
+
+}
